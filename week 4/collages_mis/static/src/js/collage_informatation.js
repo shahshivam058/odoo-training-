8ea6559fc0a5e205caa42307ae0fbf63c9fa4a7d
shivam shah <53250099+shs-odoo@users.noreply.github.com>
2020-10-16 14:37:29 +0530

    [IMP] Improve code

--- week 4/collages_mis/static/src/js/collage_informatation.js
+++ week 4/collages_mis/static/src/js/collage_informatation.js
@@ -1,65 +1,64 @@
-odoo.define('collages_mis.ClientAction', function (require) {
-"use strict";
+odoo.define('collages_mis.ClientAction', function(require) {
+    "use strict";
 
-var core = require('web.core');
-var AbstractAction = require('web.AbstractAction');
-var Widget = require('web.Widget');
-var collageTemplate = AbstractAction.extend({
-    template: "collage_template",
+    var core = require('web.core');
+    var AbstractAction = require('web.AbstractAction');
+    var Widget = require('web.Widget');
+    var collageTemplate = AbstractAction.extend({
+        template: "collage_template",
 
-    events: {
-        'click .add' : 'addnew'
-    },
-
-     start: function(){
-       this._super.apply(this, arguments);
-       this.addnew()
-
-    },
-
-    addnew : function (){
-        var counter = new Counter(this,0);
-        counter.appendTo('.container');
-
-    }
-
-});
-
-var Counter = Widget.extend({
-       xmlDependencies: ['/static/src/xml/collage_tamplate.xml'],  
-       template: "collage_widget",   
-        
         events: {
-            "click .increment": 'onClickincrement',
-            'click .decrement' : 'onClickdecrement',
-            'click .destroy' : 'onClickdestroy'
+            'click .add': 'addnew'
         },
 
-        init: function (parent, value) {
+        start: function() {
+            this._super.apply(this, arguments);
+            this.addnew()
+
+        },
+
+        addnew: function() {
+            var counter = new Counter(this, 0);
+            counter.appendTo('.container');
+
+        }
+
+    });
+
+    var Counter = Widget.extend({
+        xmlDependencies: ['/static/src/xml/collage_tamplate.xml'],
+        template: "collage_widget",
+
+        events: {
+            "click .increment": 'onClickincrement',
+            'click .decrement': 'onClickdecrement',
+            'click .destroy': 'onClickdestroy'
+        },
+
+        init: function(parent, value) {
             this._super.apply(this, arguments);
             this.count = value;
         },
 
-        onClickincrement: function () {
+        onClickincrement: function() {
             this.count++;
             this.$('.count-number').val(this.count);
         },
 
-        onClickdecrement : function () {
+        onClickdecrement: function() {
             this.count--;
             this.$('.count-number').val(this.count);
         },
 
-        onClickdestroy : function()
-        {
+        onClickdestroy: function() {
             this.destroy()
 
         }
 
-});
+    });
 
-core.action_registry.add('collage_action', collageTemplate);
-    
+    core.action_registry.add('collage_action', collageTemplate);
+
     return collageTemplate;
 
 
